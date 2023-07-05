ae4c3d4a158af798db162c0b223fcad0cb164a4c
OliverE <mail@olivereckert.de>
2023-07-05 18:19:42 +0200

    post, put and delete enabled

--- app.js
+++ app.js
@@ -1,47 +1,92 @@
+const mysql = require("mysql");
+const pool = mysql.createPool({
+    host: "",
+    user: "",
+    password: "",
+    database: "",
+    connectionLimit: 20,
+})
 const express = require("express");
 const cors = require("cors");
-
-const mockData = require("./mockData.json");
-
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 3306;
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/", function (req, res) {
-
-    res.json(mockData);
+app.get("/", (req, res) => {
+    pool.query("SELECT * FROM myTravelBlog", (error, results, fields) => {
+        if (error) {
+            console.log("error");
+            res.status(500).send({ ok: false, error: error });
+        } else {
+            res.send({ ok: true, result: results });
+        }
+    })
+});
+app.get("/:id", (req, res) => {
+    pool.query(`SELECT * FROM myTravelBlog WHERE id=?`,
+        req.params.id,
+        (error, results, fields) => {
+            if (error) {
+                console.log("error");
+                res.status(500).send({ ok: false, error: error });
+            } else {
+                res.send({ ok: true, result: results });
+            }
+        });
+});
+app.put("/:id", (req, res) => {
+    pool.query(
+        `UPDATE myTravelBlog SET author= ? WHERE id= ?`,
+        [req.body.editedPost, req.body.index],
+        console.log("Now I#m here!"),
+        (error, results, fields) => {
+            if (error) {
+                console.log("error");
+                res.status(500).send({ ok: false, error: error });
+            } else {
+                console.log("some shit and no put happened?")
+                res.send({ ok: true, result: results });
+                console.log(res)
+            }
+        }
+    );
+    res.json("Thanks for updating your post");
+});
+app.delete("/", (req, res) => {
+    pool.query(
+        `DELETE FROM myTravelBlog WHERE id= ?`,
+        req.body.index,
+        (error, results, fields) => {
+            if (error) console.log(error);
+            return showCurrentStatus(res);
+        });
 });
 
-app.get("/location", function (req, res) {
-    const getPos = req.query.position;
-    const location = mockData[getPos];
-
-    if (location) {
-        res.json(location);
-    } else {
-        res.json("Location not found");
-    }
-});
-
-app.get("/delete", function (req, res) {
-    console.log("THERE WAS A DELETE REQUEST FOR LOCATION")
-    const getPos = req.query.position;
-    mockData[getPos] = undefined;
-
-    res.json("Location deleted");
-});
-
-app.post("/newlocation", function (req, res) {
-    console.log("THERE WAS A POST REQUEST FOR NEW LOCATION");
-    console.log(req.body);
-
-    mockData.push(req.body);
-
+app.post("/", (req, res) => {
+    const dataToInsert = req.body;
+    console.log(`posting: ${req.params.id}`)
+    pool.query(
+        "INSERT INTO myTravelBlog SET ?",
+        dataToInsert,
+        (error, results, fields) => {
+            if (error)
+                console.log(error);
+        }
+    );
     res.json("Thanks");
 });
-
+showCurrentStatus = (res) => {
+    pool.query("SELECT * FROM myTravelBlog", (error, results, fields) => {
+        if (error) {
+            console.log(error);
+            res.status(500).send({ ok: false, error: error });
+        } else {
+            res.send({ myTravelBlog: results });
+        }
+    });
+};
 app.listen(port, function () {
-    console.log(`Running on port ${port}`);
+    console.log(`Running smoothly on port ${port}`);
 });
